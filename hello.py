03c5012721d9c688c3b5dd60ebc1222d1a124b89
Bruna Teixeira <stox@DESKTOP-GTIEIRK.>
2025-04-27 21:07:15 -0300

    implementamos o hello world multi linguas

--- hello.py
+++ hello.py
@@ -1,4 +1,39 @@
-print ("Hello, World!")
+#!usr/bin/env python3
+"""hello world multi linguas.
+
+dependendo da lingua configurada no ambiente o programa exibe a mensagem 
+correspondente.
+
+como usar:
+
+tenha a variavel LANG devidamente configurada ex:
+
+    export LANG=pt_BR
+    
+execução:
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+__version__ ="0.0.1"
+__author__ ="Bruna Teixeira"
+__license__ ="Unlicense"
+
+
+import os
+
+current_language = "it_IT"
+
+msg = "hello, world!"
+
+if current_language =="pt_BR":
+   msg = "Olá mundo!"
+
+elif current_language =="it_IT":
+    msg = "Ciao, Mondo!"
+
+print(msg)
+# não consigo acessar a pasta
 print ('bruna'.upper())
 print ("頑張って～")
 print("esse é meu primeiro comando")
