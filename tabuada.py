f4cbd0253f23a4065e02c2929112408b3c0ca24d
Bruna Teixeira <stox@DESKTOP-GTIEIRK.>
2025-05-05 18:18:38 -0300

    Atualiza script da tabuada

--- tabuada.py
+++ tabuada.py
@@ -1,29 +1,32 @@
 #!/usr/bin/env python
 """Imprime a tabuada do 1 ao 10.
 
-Tabuada do 1
-1
-2
-3
+---Tabuada do 1---
+    1 x 1 = 1
+    2 x 1 = 2
+    3 x 1 = 3
 ...
--------------
-Tabuada do 2
-2
-3
-4
-
-
+##################
+---Tabuada do 2---
+    2 x 1 = 2
+    2 x 2 = 4
+    2 x 3 = 6
+...
+#################
 """
-__version__ ="0.1.0"
+__version__ ="0.1.1"
 __author__ = "Bruna"
-# base = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# Iterable (percorriveis)
 numeros = list(range(1,11))
 
-# Iterable (objetos percorriveis)
-
 # para cada numero dentro da sequencia numeros:
-for numero in numeros:
-    print("Tabuada do:", numero)
-    for outro_numero in numeros:
-        print(numero * outro_numero)
-    print("----------")
+for n1 in numeros:
+    print("{:-^18}".format(f"Tabuada do {n1}"))
+    print()
+    for n2 in numeros:
+        resultado = n1 * n2
+        print("{:^18}".format(f"{n1} x {n2} = {resultado}"))
+         
+    print("#" * 18)
