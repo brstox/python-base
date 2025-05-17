b989e9e6f416082ab51b414f5ddd4af08cb79475
brstox <brunastox@gmail.com>
2025-05-17 17:06:10 -0300

    Adiciona novas alterações de linguas

--- hello.py
+++ hello.py
@@ -1,30 +1,42 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 """hello world multi linguas.
-
-dependendo da lingua configurada no ambiente o programa exibe a mensagem 
-correspondente.
-
+dependendo da lingua configurada no ambiente o programa exibe a mensagem correspondente
 como usar:
-
-tenha a variavel LANG devidamente configurada ex:
-
-    export LANG=pt_BR
-    
-execução:
-    python3 hello.py
-    ou
-    ./hello.py
+tenha a variavel Lang devidamente configurada ex:
+    export Lang-pt_BR
+ou informe atraves do CLI argument '--lang'
+ou o usuario tera que digitar
 """
-__version__ ="0.1.2"
-__author__ ="Bruna Teixeira"
-__license__ ="Unlicense"
 
+__version__ = "0.1.3"
+__author__ = "Bruna Teixeira"
+__license__ = "Unlicense"
 
 import os
+import sys
 
-current_language = os.getenv("LANG", "en_US")[:5]
+arguments = {"lang": None, "count": 1,}
 
-msg ={
+for arg in sys.argv[1:]:
+    if "=" in arg: #adicionado de acordo com o deep para rodar
+        key, value = arg.split("=")
+        key = key.lstrip("-").strip()
+        value = value.strip()
+        if key not  in arguments:
+            print(f"Invalid option '{key}'")
+            sys.exit()
+        arguments[key] = value
+
+current_language = arguments["lang"]
+if current_language is None:
+    if "LANG" in os.environ:
+         current_language = os.getenv("LANG")
+    else:
+        current_language = input("Choose a language:")
+
+current_language = current_language[:5]
+
+msg = {
     "en_US": "Hello, World!",
     "pt_BR": "Olá, Mundo!",
     "it_IT": "Ciao, Mondo!",
@@ -32,4 +44,4 @@ msg ={
     "fr_FR": "Bonjour, Monde!"
 }
 
-print(msg[current_language])
+print(msg[current_language] * arguments["count"])
