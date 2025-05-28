00843ad802f468acec6073faeedf313abf2b27a2
brstox <brunastox@gmail.com>
2025-05-28 15:46:19 -0300

    adiciona emails

--- interpolacao.py
+++ interpolacao.py
@@ -1,35 +1,39 @@
- email_tmpl = """
-    ...: Olá, %(nome)s
-    ...:
-    ...: Tem interesse em comprar %(produto)s
-    ...:
-    ...: Este produto é ótimo para resolver %(texto)s
-    ...:
-    ...: Clique agora %(link)s
-    ...:
-    ...: Apenas %(quantidade)d disponíveis:
-    ...:
-    ...: Preço promocional %(preço).2f
-    ...: """
-    ...:
-    ...: clientes = ["Janaína", "Laura", "Helena"]
-    ...:
-    ...: texto_descritivo = """Problemas de segurança? Na PentaSeg, nossa missão é transformar sua casa em um ambiente seguro e intel
-       ⋮ igente. Com soluções personalizadas em sistemas de segurança residencial, oferecemos tecnologia avançada para garantir tranq
-       ⋮ uilidade para você e sua família.
-    ...:
-    ...: Desenvolvemos desde câmeras de monitoramento 24/7 até sistemas de alarme integrados, tudo controlado pelo seu smartphone. Co
-       ⋮ mbinando inovação e confiabilidade, a PentaSeg está sempre um passo à frente, protegendo o que mais importa.
-    ...:
-    ...: Sua segurança em primeiro lugar. Sempre."""
-    ...:
-    ...: for cliente in clientes:
-    ...:     print(email_tmpl % {
-    ...:         "nome": cliente,
-    ...:         "produto": "Sistema de segurança residencial",
-    ...:         "texto": texto_descritivo,
-    ...:         "link": "https://pentaseg.com",
-    ...:         "quantidade": 1,
-    ...:         "preço": 15000.00
-    ...:     })
+#!/user/bin/env python
+"""Imprime amensagem de u e-mail
+"""
+__version__="0.1.1"
 
+import sys
+import os
+
+arguments = sys.argv[1:]
+if not arguments:
+    print ("informe o nome do arquivo de emails")
+    sys.exit(1)
+
+filename = arguments[0]
+templatename = arguments[1]
+path = os.curdir
+filepath = os.path.join(path, filename) # emails.tx
+templatepath = os.path.join(path,templatename) #emails_tmpl.txt
+
+clientes= []
+for line in open(filepath):
+    name, email = line.split(",")
+    
+    #TODO: Substituir por envio de email
+    print(f"Enviando email para: {email}")
+    print()
+    print(
+        open(templatepath).read()
+        % {
+            "nome": name,
+            "produto": "caneta",
+            "texto": "escrever muito bem",
+            "link": "https://pentaseg.com",
+            "quantidade": 1,
+            "preço": 50.5,
+
+        }
+    )
+print("-" * 50)
